18c5b2b2ed9c9f3b180cbfc7605fa556383435b7
Andriely <00001138008370sp@al.educacao.sp.gov.br>
2025-09-05 10:15:03 -0300

    Trabalhos com funções e eventos no javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,52 +8,75 @@ const btnLoginOpen = document.querySelector("#btn-login-open");
 const inputEmail = document.querySelector("#email");
 const resultado = document.querySelector("#resultado");
 
-function soma(num01, num02) {
-    const somar = num01 + num02;
-    console.log(somar); 
+const pegar_valor = (valor) => {
+  resultado.innerHTML = `
+    <div class="valor">
+      <h1>${valor}</h1>
+    </div>
+  `
 }
 
-soma(5, 2)
+function soma(num01, num02) {
+  const somar = num01 + num02;
+  console.log(somar);
+}
+
+soma(5, 2);
 
 // arrow function
 const multiplicar = (num01, num02) => {
-    return num01 * num02 
-}
+  return num01 * num02;
+};
 
 const dividir = (num01, num02) => {
-    return num01 / num02 
-}   
+  return num01 / num02;
+};
 
 console.log(multiplicar(5, 8));
 console.log(dividir(5, 2));
 
 // addEventListener => Escuta o evento de click e executa a função
 btnLogin.addEventListener("click", (e) => {
-    modalLogin.classList.remove("hide-modal");
-    modalLogin.classList.add("show-modal");
-})
+  modalLogin.classList.remove("hide-modal");
+  modalLogin.classList.add("show-modal");
+});
 
 btnRegister.addEventListener("click", (e) => {
-    modalRegister.classList.remove("hide-modal");
-    modalRegister.classList.add("show-modal");
-})
+  e.preventDefault();
+  modalRegister.classList.remove("hide-modal");
+  modalRegister.classList.add("show-modal");
+});
 
 btnCancelLogin.addEventListener("click", (e) => {
-    modalLogin.classList.remove("show-modal");
-    modalLogin.classList.add("hide-modal");
-
-})
+  e.preventDefault();
+  modalLogin.classList.remove("show-modal");
+  modalLogin.classList.add("hide-modal");
+  inputEmail.value = "";
+  resultado.innerHTML = "";
+  inputEmail.style.borderBottom = "0.5rem solid rgb(255, 255, 255)";
+});
 
 btnCancelRegister.addEventListener("click", (e) => {
-    modalRegister.classList.remove("show-modal");
-    modalRegister.classList.add("hide-modal");
+  e.preventDefault();
+  modalRegister.classList.remove("show-modal");
+  modalRegister.classList.add("hide-modal");
+});
 
-})
-
- inputEmail.addEventListener("input", () => {
-    resultado.innerHTML = inputEmail.value
-})
+// inputEmail.addEventListener("input", () => {
+//   resultado.innerHTML = inputEmail.value;
+// });
 
 inputEmail.addEventListener("focus", () => {
-    inputEmail.style.borderBottom = "0.5rem solid rgb(46, 21, 138)"
+  inputEmail.style.borderBottom = "0.5rem solid rgb(46, 21, 138)";
+});
+
+btnLoginOpen.addEventListener("click", (e) => {
+  e.preventDefault();
+  const email = inputEmail.value;
+  pegar_valor(email);
+});
+
+inputEmail.addEventListener("focusout", () => {
+  inputEmail.style.borderBottom = "0.5rem solid rgb(255, 255, 255)";
 })
+
